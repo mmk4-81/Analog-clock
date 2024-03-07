d60f8e590336967bdc17ee6ab44dc5c2d367eabc
mmk <mmkkarbalaei@gmail.com>
2024-03-07 15:57:15 +0330

    done

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 let sec = document.querySelector('.sec');
+let min = document.querySelector('.min');
+let hour = document.querySelector('.hour');
+
 let deg = 6;
 
 setInterval(ClockHandler,1000);
 
 function ClockHandler(){
-    sec.style.transform = `rotateZ(${deg}deg)`;
-    deg+=6;
+    let d = new Date();
+    let h = d.getHours()*30;
+    let m = d.getMinutes()*deg;
+    let s = d.getSeconds()*deg;
+    sec.style.transform = `rotateZ(${s}deg)`;
+    min.style.transform = `rotateZ(${m}deg)`;
+    hour.style.transform = `rotateZ(${h + (m/12)}deg)`;
 }
+
+
